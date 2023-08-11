628e3f787850b5b9078beb9c3d5202266da0af48
Kyle Tan <120530858+ktan46@users.noreply.github.com>
2023-08-11 18:18:55 -0400

    Fixed sentence in TempFooter.tsx

--- components/TempFooter.tsx
+++ components/TempFooter.tsx
@@ -5,7 +5,7 @@ import { LuConstruction } from 'react-icons/lu'
 export default function Footer() {
   return (
     <section className="flex mb-5 bg-yellow-200 blur hover:blur-none transition">
-      <h3 className="text-3xl font-medium">Ah! This Section is Still Under Construction{"  "}</h3>
+      <h3 className="text-3xl font-medium">Ah! This Section is Still Under Construction.{"  "}</h3>
       <div className="mt-2 pl-3 text-xl">
         <LuConstruction/>
       </div>
